194935212aa976d59fa18174d7ea37367f848485
Johan Uribe <johan.uribe@gmail.com>
2026-04-02 21:21:40 -0500

    Setting up files separated

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "Andy Weir",
+    title: "Project Hail Mary",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Andam Wallace",
+    title: "How to Catch the Easter Bunny",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Allen Levi",
+    title: "Theo of Golden: A Novel",
+    img: img3,
+    id: 3,
+  },
+];
